ed66291e1ac525533b54c106375dce0cf8469dea
nbeausejour <nicolas.beausejour@gmail.com>
2017-09-05 13:40:50 -0400

    Add files via upload

---
+++ content.js
@@ -0,0 +1,22 @@
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/(Daniel)/gi, 'Drangle');
+            var replacedText = text.replace(/(Bingo)/gi, 'Bringo');
+            var replacedText = text.replace(/(Sushi)/gi, 'Shushi');
+            var replacedText = text.replace(/(Skateboard)/gi, 'Skrateboard');
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+                   
+            }
+        }
+    }
+}
