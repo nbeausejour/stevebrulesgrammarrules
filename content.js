14bec85b1c4bbc67ba1f2bc3bfc2b1f7c4d990af
nbeausejour <nicolas.beausejour@gmail.com>
2017-09-05 13:44:30 -0400

    Update content.js

--- content.js
+++ content.js
@@ -12,6 +12,7 @@ for (var i = 0; i < elements.length; i++) {
             var replacedText = text.replace(/(Bingo)/gi, 'Bringo');
             var replacedText = text.replace(/(Sushi)/gi, 'Shushi');
             var replacedText = text.replace(/(Skateboard)/gi, 'Skrateboard');
+            var replacedText = text.replace(/(Airplane)/gi, 'Jumbo Jret');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
